e46d126a9aa88b7004ba63c739aa9587951a07ba
Jellybin06 <pjbboseung0817@gmail.com>
2023-11-23 12:48:57 +0900

    xss1 xss2

--- xss/blacklist_xss-2/app.py
+++ xss/blacklist_xss-2/app.py
@@ -11,9 +11,7 @@ def filter_query(query):
 
 @app.route("/", methods=["GET"])
 def index():
-    
     # 플래그 값은 쿠키에 저장하지 않고 직접 템플릿에 전달
-    
     return render_template("index.html", flag=FLAG)
 
 @app.route("/set_cookie")
@@ -22,18 +20,18 @@ def set_cookie():
     response.set_cookie("ctf_cookie", FLAG)
     return response
 
-@app.route("/execute_query", methods=["POST"])
+@app.route("/execute_query", methods=["GET"])
 def execute_query():
-   # POST 요청에서 입력값 받기
-   query = request.form.get("query", "")
-   filter = "filter ^!^"
+    # POST 요청에서 입력값 받기
+    query = request.args.get("query", "")
    
-   forbidden_strings = ["script", "on", "javascript", "window", "self", "location"]
-   for forbidden_str in forbidden_strings:
+    forbidden_strings = ["iframe", "src", "script", "on", "window", "href", "javascript"]
+    for forbidden_str in forbidden_strings:
         query = query.replace(forbidden_str, "")
-   
-   # 필터링 없이 쿼리를 템플릿에 전달
-   return render_template("index.html", flag=FLAG, query=query)
+    # 필터링 없이 쿼리를 템플릿에 전달하고 safe 필터를 사용하여 이스케이프 피하기
+    return render_template("index.html", flag=FLAG, query=Markup(query))
 
 if __name__ == "__main__":
     app.run(debug=True, port=8001)
+
+
